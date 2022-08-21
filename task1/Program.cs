5f9199c66456893b2f04455d074beda6d2823a6a
Dmitriy B <kupperfeet@gmail.com>
2022-08-21 23:23:16 +0300

    task 1 Arraybase

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,23 @@
 ﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+void FillArray(int[] array)
+{
+    int index = 0;
+    var random = new Random();
+    while (index < array.Length)
+    {
+        array[index] = random.Next(0, 2);
+        index++;
+    }
+}
+void PrintArray(int[] collection)
+{
+    int position = 0;
+    while (position < collection.Length)
+{
+    Console.Write(collection[position] +" ");
+    position++;
+}
+}
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
