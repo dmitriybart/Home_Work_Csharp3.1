8e5b1412128735ce48556ef35b6236f2daeb106a
Dmitriy B <kupperfeet@gmail.com>
2022-08-22 03:16:35 +0300

    task 3 done repl

---
+++ task3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Написать программу замену элементов массива на противоположные
+int [] FillArray(int size, int minimum, int maximum)
+{
+    int[] array = new int [size];
+    int index = 0;
+    var random = new Random();
+    while (index < array.Length)
+    {
+        array[index] = random.Next(minimum, maximum+1);
+        index++;
+    }
+    return array;
+    }
+void PrintArray(int[] collection)
+{
+    int position = 0;
+    while (position < collection.Length)
+{
+    Console.Write(collection[position] +" ");
+    position++;
+}
+Console.WriteLine();
+}
+void ReArray(int[] replacement)
+{
+    int i = 0;
+    while (i < replacement.Length)
+    {
+        replacement[i]=-replacement[i];
+        
+        i++;
+    }
+}
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите минимальное значение: ");
+int minimum = int.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите максимальное значение: ");
+int maximum = int.Parse(Console.ReadLine()?? "0");
+int [] array = FillArray(size, minimum, maximum);
+PrintArray(array);
+ReArray(array);
+PrintArray(array);
